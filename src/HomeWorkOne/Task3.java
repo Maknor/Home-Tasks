f0fe6ee909554026559beeb25e5d93fab5bc11a3
Vladislav Fieshko <maknor1995@yandex.ru>
2020-03-22 23:20:59 +0300

    All tasks is ready

--- src/HomeWorkOne/Task3.java
+++ src/HomeWorkOne/Task3.java
@@ -3,18 +3,18 @@ package HomeWorkOne;
 public class Task3 {
     public static void main(String[] args) {
         double a=-3.5;
-        double b=18.45;
+        double b=27.45;
         int x=10;
         double a1=Math.abs(x-a);
         double b1=Math.abs(x-b);
 
-        if (a1==b1){
+        if (a1 == b1) {
             System.out.println("Числа "+a +" и "+b + " равноудалены от "+x);
         }
-        if(a1>b1){
+        else if (a1 > b1) {
             System.out.println("Число "+b+" ближе к "+x);
         }
-        if (a1<b1) {
+        else if (a1 < b1) {
             System.out.println("Число "+a+" ближе к "+x);
         }
     }
