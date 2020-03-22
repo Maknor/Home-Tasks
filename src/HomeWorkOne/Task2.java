e75ae44881c786017d5bb02496fc5d63d9727180
Vladislav Fieshko <maknor1995@yandex.ru>
2020-03-22 21:09:17 +0300

    Обновление 2

--- src/HomeWorkOne/Task2.java
+++ src/HomeWorkOne/Task2.java
@@ -2,6 +2,6 @@ package HomeWorkOne;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        int a = 45;
     }
 }
--- src/HomeWorkOne/Task3.java
+++ src/HomeWorkOne/Task3.java
@@ -2,6 +2,6 @@ package HomeWorkOne;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        int a = 45;
     }
 }
