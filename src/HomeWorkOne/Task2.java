e58903f3dad56e49b96c360a8e0875dc894a77df
Vladislav Fieshko <maknor1995@yandex.ru>
2020-03-22 21:22:38 +0300

    Task2 is ready

--- src/HomeWorkOne/Task2.java
+++ src/HomeWorkOne/Task2.java
@@ -2,6 +2,25 @@ package HomeWorkOne;
 
 public class Task2 {
     public static void main(String[] args) {
-        int a = 45;
+        int x = 15;
+        int y = 25;
+        int z = 5;
+
+            if (x < y && x < z) {
+                if (y < z)
+                    System.out.println("Последовательность: " + x + ", " + y + ", " + z);
+                else
+                    System.out.println("Последовательность: " + x + ", " + z + ", " + y);
+            } else if (y < x && y < z) {
+                if (x < z)
+                    System.out.println("Последовательность: " + y + ", " + x + ", " + z);
+                else
+                    System.out.println("Последовательность: " + y + ", " + z + ", " + x);
+            } else {
+                if (x < y)
+                    System.out.println("Последовательность: " + z + ", " + x + ", " + y);
+                else
+                    System.out.println("Последовательность: " + z + ", " + y + ", " + x);
+            }
     }
 }
