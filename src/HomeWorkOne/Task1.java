a0e45e362e41ee602a0d9134ccea5211e111ef96
Vladislav Fieshko <maknor1995@yandex.ru>
2020-03-22 21:03:29 +0300

    Task1 is ready

---
+++ src/HomeWorkOne/Task1.java
@@ -0,0 +1,12 @@
+package HomeWorkOne;
+
+public class Task1 {
+    public static void main(String args[]) {
+        int a = 55;
+
+        if (25 <= a && a <= 100)
+            System.out.println("Указанное число " + a + " - попало в интервал (25;100)");
+        else
+            System.out.println("Указанное число " + a + " - не попало в интервал (25;100)");
+    }
+}
