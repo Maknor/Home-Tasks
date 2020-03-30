87718a742d7d5153f26632d43a5fb39d7aaa6dbb
Vladislav Fieshko <maknor1995@yandex.ru>
2020-03-30 08:38:19 +0300

    Add Homework week2

--- src/HomeWorkOne/Task2.java
+++ src/HomeWorkOne/Task2.java
@@ -6,21 +6,21 @@ public class Task2 {
         int b = 25;
         int c = 5;
 
-            if (a < b && a < c) {
-                if (b < c)
-                    System.out.println("Последовательность: " + a + ", " + b + ", " + c);
-                else
-                    System.out.println("Последовательность: " + a + ", " + c + ", " + b);
-            } else if (b < a && b < c) {
-                if (a < c)
-                    System.out.println("Последовательность: " + b + ", " + a + ", " + c);
-                else
-                    System.out.println("Последовательность: " + b + ", " + c + ", " + a);
-            } else {
-                if (a < b)
-                    System.out.println("Последовательность: " + c + ", " + a + ", " + b);
-                else
-                    System.out.println("Последовательность: " + c + ", " + b + ", " + a);
-            }
+        if (a < b && a < c) {
+            if (b < c)
+                System.out.println("Последовательность: " + a + ", " + b + ", " + c);
+            else
+                System.out.println("Последовательность: " + a + ", " + c + ", " + b);
+        } else if (b < a && b < c) {
+            if (a < c)
+                System.out.println("Последовательность: " + b + ", " + a + ", " + c);
+            else
+                System.out.println("Последовательность: " + b + ", " + c + ", " + a);
+        } else {
+            if (a < b)
+                System.out.println("Последовательность: " + c + ", " + a + ", " + b);
+            else
+                System.out.println("Последовательность: " + c + ", " + b + ", " + a);
+        }
     }
 }
---
+++ src/HomeWorkTwo/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package HomeWorkTwo;
+
+public class MultiplicationTable {
+
+    public static void Table() {
+        System.out.println("Таблица умножения:");
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.print(i * j + " ");
+                if(i * j < 10) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
